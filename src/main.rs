4631e79b26f42e5e56b09a25ac86c8032e030689
threeh <threeh1@vk.com>
2024-07-23 22:45:26 +0300

    теперь еще файлики можно качать)

--- src/main.rs
+++ src/main.rs
@@ -8,12 +8,14 @@ use http_body_util::Full;
 use std::net::{SocketAddr};
 use tokio::io::AsyncWriteExt;
 use tokio::net::{TcpListener};
-use hyper::{Request, Response};
+use hyper::{Request, Response, StatusCode};
 use hyper::server::conn::http1;
 use hyper::service::service_fn;
 use hyper_util::rt::{TokioIo};
 use crate::config::server_config::{Config, Server};
 use std::collections::HashMap;
+use std::{env, fs};
+use std::path::Path;
 use uuid::{ContextV7, Timestamp, Uuid};
 
 #[tokio::main]
@@ -28,6 +30,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         let port = String::from(&server_conf.listen).parse()?;
         let addr: SocketAddr = SocketAddr::from(([127, 0, 0, 1], port));
         let listener: TcpListener = TcpListener::bind(&addr).await?;
+        let root: String = String::from(&server_conf.root);
 
         let task = tokio::spawn(async move {
             loop {
@@ -60,5 +63,24 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 }
 
 async fn handle_connection(request: Request<impl hyper::body::Body>) -> Result<Response<Full<Bytes>>, Infallible> {
-    Ok(Response::new(Full::new(Bytes::from("Hello World!"))))
+    let base_path = Path::new("/home/threeh/test");
+    let request_path = Path::new(request.uri().path());
+
+    let request_path_str = request_path.to_str().expect("Invalid Unicode in path");
+
+    let request_path_str = if request_path_str.starts_with('/') {
+        &request_path_str[1..]
+    } else {
+        request_path_str
+    };
+
+    let full_path = base_path.join(Path::new(request_path_str));
+
+    match fs::read(full_path) {
+        Ok(contents) => Ok(Response::new(Full::from(Bytes::from(contents)))),
+        Err(_) => Ok(Response::builder()
+            .status(StatusCode::NOT_FOUND)
+            .body(Full::from("File not found"))
+            .unwrap()),
+    }
 }
